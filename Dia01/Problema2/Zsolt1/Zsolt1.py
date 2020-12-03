55f6ae81d6abe9f3e78d5c66c7000d6b8834c762
zsoltpalfi <zsolt.palfi@estudiantat.upc.edu>
2020-12-03 22:47:28 +0100

    zsolt problem2 day1

---
+++ Dia01/Problema2/Zsolt1/Zsolt1.py
@@ -0,0 +1,15 @@
+archivo = open("Dia01\Problema2\Zsolt1\input.txt","r")
+
+if archivo.mode == "r":
+    numeros = archivo.readlines()
+
+for linea1 in numeros:
+    for linea2 in numeros:
+        for linea3 in numeros:
+            numlinea1 = int(linea1)
+            numlinea2 = int(linea2)
+            numlinea3 = int(linea3)
+            if linea1 != linea2 and linea2 != linea3:
+                if (numlinea1+numlinea2+numlinea3==2020):
+                    print("num1= ", numlinea1, "num2= ", numlinea2, "num3= ", numlinea3)
+                    print("solucion= ",numlinea1*numlinea2*numlinea3)
