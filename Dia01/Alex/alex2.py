9e2e0ffa49b57ec77b0dc6d6d39a082482f4e558
a96lex <a96lex@gmail.com>
2020-12-03 23:48:42 +0100

    updated folder structure again

--- Dia01/Alex/alex2.py
+++ Dia01/Alex/alex2.py
@@ -3,5 +3,6 @@ with open("pr1.dat") as f:
 
 for i in range(len(nums)):
     for j in range(i, len(nums)):
-        if nums[i] + nums[j] == 2020:
-            print(nums[i] * nums[j], nums[i], nums[j])
+        for k in range(j, len(nums)):
+            if nums[i] + nums[j] + nums[k] == 2020:
+                print(nums[i] * nums[j] * nums[k], nums[i], nums[j], nums[k])
