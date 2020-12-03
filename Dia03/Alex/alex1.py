35a9af997a96787dc68e2b33a03fd0105af3c0ca
a96lex <a96lex@gmail.com>
2020-12-04 00:06:38 +0100

    d3 p2 alex

--- Dia03/Alex/alex1.py
+++ Dia03/Alex/alex1.py
@@ -3,11 +3,25 @@ with open("day3") as f:
 
 size = len(trees[0])
 
-tcount = 0
-for i in range(len(trees)):
-    n = i * 3 - size * ((3 * i) // size)
-    print(n, trees[i][n])
-    if trees[i][n] == "#":
-        tcount += 1
 
-print(tcount)
+def slopes(mov):
+    dx = mov[0]
+    dy = mov[1]
+    tcount = 0
+    for i in range(len(trees) / dy):
+        n = i * dx - size * ((dx * i) // size)
+        if trees[dy * i][n] == "#":
+            tcount += 1
+    if dy > 1:
+        tcount += 1
+    return tcount
+
+
+slope = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+
+end = 1
+
+for s in range(len(slope)):
+    end *= slopes(slope[s])
+
+print(end)
