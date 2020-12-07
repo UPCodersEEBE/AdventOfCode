5269e3646945ee79eb80da7f02ebcd69c71063a1
totimorpa <totimorpa@gmail.com>
2020-12-07 15:43:56 +0100

    problema 1/2 dia7

--- Dia07/Toti/day7.py
+++ Dia07/Toti/day7.py
@@ -5,34 +5,47 @@ for i in l:
     rules[i.split(" contain ")[0]] = i.split(" contain ")[1].split(", ")
 
 
-def RemoveNumberAndDotFromString(str):
+def ReturnNumberAndCleanString(str):
+    num = ""
     x = ""
     for i in str:
         if i in "abcdefghijklmnopqrstuvwxyz ":
             x += i
+        if i in "0123456789":
+            num += i
     if x[:2] == "no":
-        return(x)
+        return(x, 0)
     if x[-1] == "g":
         x += "s"
-    return(x[1:])
+    return(x[1:], int(num))
 
 def ContainsShinyGold(bag):
     # print("IN " + str(bag))
     # print("----------")
     for i in rules[bag]:
         # print(RemoveNumberAndDotFromString(i))
-        if RemoveNumberAndDotFromString(i) == "shiny gold bags":
+        if ReturnNumberAndCleanString(i)[0] == "shiny gold bags":
             # print("FOUND! +1")
             return True
-        elif RemoveNumberAndDotFromString(i) == "no other bags":
+        elif ReturnNumberAndCleanString(i)[0] == "no other bags":
             return False
         else:
             # print("---------->")
-            if ContainsShinyGold(RemoveNumberAndDotFromString(i)):
+            if ContainsShinyGold(ReturnNumberAndCleanString(i)[0]):
                 return True
             else:
                 continue
     return False
+
+def IndividualBagsInsideBag(bag):
+    c = 0
+    for i in rules[bag]:
+        if ReturnNumberAndCleanString(i)[0] != "no other bags":
+            c += ReturnNumberAndCleanString(i)[1] + ReturnNumberAndCleanString(i)[1] * (IndividualBagsInsideBag(ReturnNumberAndCleanString(i)[0]))
+    
+    return(c)
+
+
 
 c = 0
 for i in rules:
@@ -46,3 +59,4 @@ for i in rules:
 
 print(c)
 
+print(IndividualBagsInsideBag("shiny gold bags"))
