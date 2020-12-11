2dc82db319270853f617a4ffc044f5c6cb57965d
clara <clara.rull@estudiantat.upc.edu>
2020-12-11 15:22:54 +0100

    problem 2 dia 2

---
+++ Dia02/Clara/ReadFile2.java
@@ -0,0 +1,81 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+import java.util.regex.Pattern; //regex package
+import java.util.regex.Matcher; //idk difference yet
+import org.apache.commons.lang3.StringUtils;
+
+public class ReadFile2 {
+  public static void main(String[] args) {
+    try {
+      File myObj = new File("input1.txt");
+      Scanner myReader = new Scanner(myObj);
+      int numOfPasswords=0;
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        //System.out.println(Pattern.matches("[0-9]{1,2}", data));
+
+        Pattern p = Pattern.compile("[0-9]{1,2}");
+        Matcher m = p.matcher(data);
+
+
+        int min = 0;
+        int max = 0;
+        String letter="";
+        String password="";
+
+        if (m != null){
+            m.find();//true
+            min=Integer.parseInt(m.group());
+
+        }
+
+        Pattern pp = Pattern.compile("(?<=-)[0-9]{1,2}");
+        Matcher mm = pp.matcher(data);
+
+        if (mm != null){
+            mm.find();//true
+            max=Integer.parseInt(mm.group());
+
+        }
+
+        Pattern letterPattern = Pattern.compile("[a-z]");
+        Matcher letterMatcher = letterPattern.matcher(data);
+
+
+        if (letterMatcher != null){
+            letterMatcher.find();//true
+            letter = letterMatcher.group();
+
+        }
+
+        Pattern passwordPattern = Pattern.compile("(?<=: )[a-z]*");
+        Matcher passwordMatcher = passwordPattern.matcher(data);
+
+
+        if (passwordMatcher != null){
+            passwordMatcher.find();//true
+            password = passwordMatcher.group();
+        }
+
+        String maxLetter=password.substring(max-1, max);
+        String minLetter=password.substring(min-1, min);
+
+
+        if ((maxLetter.equals(letter) && !minLetter.equals(letter)) ||
+            (!maxLetter.equals(letter) && minLetter.equals(letter)) ){
+
+            System.out.println(data);
+            numOfPasswords++;
+        }
+
+      }
+      myReader.close();
+      System.out.println(numOfPasswords);
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+}
