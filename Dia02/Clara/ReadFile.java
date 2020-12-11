92b765c3ab3d78a9637f4a7041533cd67ee19782
clara <clara.rull@estudiantat.upc.edu>
2020-12-11 15:05:29 +0100

    clara java dia2 problema 1

--- Dia02/Clara/ReadFile.java
+++ Dia02/Clara/ReadFile.java
@@ -4,23 +4,30 @@ import java.util.Scanner; // Import the Scanner class to read text files
 
 import java.util.regex.Pattern; //regex package
 import java.util.regex.Matcher; //idk difference yet
+import org.apache.commons.lang3.StringUtils;
 
 public class ReadFile {
   public static void main(String[] args) {
     try {
       File myObj = new File("input1.txt");
       Scanner myReader = new Scanner(myObj);
+      int numOfPasswords=0;
       while (myReader.hasNextLine()) {
         String data = myReader.nextLine();
-        System.out.println(data);
         //System.out.println(Pattern.matches("[0-9]{1,2}", data));
 
         Pattern p = Pattern.compile("[0-9]{1,2}");
         Matcher m = p.matcher(data);
 
+
+        int min = 0;
+        int max = 0;
+        String letter="";
+        String password="";
+
         if (m != null){
             m.find();//true
-            System.out.println(m.group());
+            min=Integer.parseInt(m.group());
 
         }
 
@@ -29,30 +36,40 @@ public class ReadFile {
 
         if (mm != null){
             mm.find();//true
-            System.out.println(mm.group());
+            max=Integer.parseInt(mm.group());
 
         }
 
         Pattern letterPattern = Pattern.compile("[a-z]");
         Matcher letterMatcher = letterPattern.matcher(data);
 
+
         if (letterMatcher != null){
             letterMatcher.find();//true
-            System.out.println(letterMatcher.group());
+            letter = letterMatcher.group();
 
         }
 
         Pattern passwordPattern = Pattern.compile("(?<=: )[a-z]*");
         Matcher passwordMatcher = passwordPattern.matcher(data);
 
+
         if (passwordMatcher != null){
             passwordMatcher.find();//true
-            System.out.println(passwordMatcher.group());
+            password = passwordMatcher.group();
+        }
 
+        int count = StringUtils.countMatches(password,letter);
+
+        if (count<=max && count>=min){
+            System.out.println(password);
+
+            numOfPasswords++;
         }
 
       }
       myReader.close();
+      System.out.println(numOfPasswords);
     } catch (FileNotFoundException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
