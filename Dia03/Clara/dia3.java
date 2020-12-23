b11a6e68d78dd3ab2ef5ef44b194f52981c2efca
clara <clara.rull@estudiantat.upc.edu>
2020-12-23 16:41:57 +0100

    dia3 prob 1 clara

---
+++ Dia03/Clara/dia3.java
@@ -0,0 +1,36 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+public class dia3 {
+  public static void main(String[] args) {
+    try {
+      File myObj = new File("Dia03/Clara/input.txt");
+      Scanner myReader = new Scanner(myObj);
+
+      int x=0;
+      int count=0;
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data.substring(x,x+1));
+        String treeOrNot = data.substring(x,x+1);
+        if (treeOrNot.equals("#")){
+          System.out.println("Tree!");
+          count++;
+        }
+        x+=3;
+        if (x>30){
+          x=x-31;
+        }
+      }
+
+      System.out.println("Count is "+count);
+      myReader.close();
+
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+  
+}
