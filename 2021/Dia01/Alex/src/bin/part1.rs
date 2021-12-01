e2f866c3d46c414a3eaa121d082254030661da41
a96lex <a96lex@gmail.com>
2021-12-01 16:14:28 +0100

    alex answer

---
+++ 2021/Dia01/Alex/src/bin/part1.rs
@@ -0,0 +1,29 @@
+use std::fs;
+use std::io;
+use std::io::BufRead;
+use std::io::BufReader;
+
+fn main() -> io::Result<()> {
+    let filename = "src/input.dat";
+    let lines = file_to_vec(filename.to_string())?;
+
+    let mut result = 0;
+    for i in 0..lines.len() - 1 {
+        let is_higher = lines[i] < lines[i + 1];
+        let increment: i32 = is_higher.into();
+        result += increment;
+    }
+    println!("Result: {}", result);
+
+    Ok(())
+}
+
+fn file_to_vec(filename: String) -> io::Result<Vec<i32>> {
+    let file_in = fs::File::open(filename)?;
+    let file_reader = BufReader::new(file_in);
+    Ok(file_reader
+        .lines()
+        .filter_map(io::Result::ok)
+        .map(|s| s.parse().unwrap())
+        .collect())
+}
