656b74e2452d3929e2cae91245a5aab8e995b882
a96lex <a96lex@gmail.com>
2021-12-21 23:09:34 +0100

    dia 12 alex
    Co-authored-by: crisouteda <cristina.outeda.rua@gmail.com>

---
+++ 2021/Dia12/Alex/part1.py
@@ -0,0 +1,45 @@
+with open("input.dat") as f:
+    data = [l.replace("\n", "") for l in f.readlines()]
+
+connections = {}
+no_repeat_nodes = []
+
+for item in data:
+    splited = item.split("-")
+    for i in range(2):
+        if splited[i] not in connections.keys():
+            connections[splited[i]] = [splited[abs(i - 1)]]
+        else:
+            connections[splited[i]].append(splited[abs(i - 1)])
+        if splited[i].islower() and splited[i] not in no_repeat_nodes:
+            no_repeat_nodes.append(splited[i])
+
+counter = 0
+
+
+def para_alex_con_amor(path: list) -> None:
+    global counter
+    paths = []
+    for item in connections[path[-1]]:
+        new_path = [*path, item]
+        if is_ended(new_path):
+            counter += 1
+        else:
+            if is_valid(new_path):
+                paths.append(new_path)
+    for path in paths:
+        para_alex_con_amor(path)
+
+
+def is_valid(path: list) -> bool:
+    if path[-1] in no_repeat_nodes:
+        return path.count(path[-1]) <= 1
+    return True
+
+
+def is_ended(path: list) -> bool:
+    return path[-1] == "end"
+
+
+para_alex_con_amor(["start"])
+print(counter)
